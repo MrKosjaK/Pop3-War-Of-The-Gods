df6f578bf6d8c8c9dd196d33f176f0bd33b8200f
MrKosjaK <misterkosiak@gmail.com>
2019-11-29 16:12:05 +0300

    Holograms

--- scripts/UtilPortal.lua
+++ scripts/UtilPortal.lua
@@ -8,11 +8,13 @@ import(Module_Objects)
 import(Module_Draw)
 
 IPortal = {}
-IPortal.reg  = function(_level, _angle, _x, _z)
+IPortal.reg  = function(_level, _angle, _x, _z, _spr, _diff)
   local p = {}
   
   p.level = _level
   p.angle = _angle
+  p.holo_spr = _spr
+  p.holo_diff = _diff
   p.coord = MAP_XZ_2_WORLD_XYZ(_x, _z)
   p.activetransfer = false
   p.transferdelay = 0
@@ -21,13 +23,26 @@ IPortal.reg  = function(_level, _angle, _x, _z)
     return p.level
   end
   
-  function p:initialize()
+  function p:init_portal()
     local portal = createThing(T_SCENERY,M_SCENERY_TOP_LEVEL_SCENERY,TRIBE_HOSTBOT,p.coord,false,false)
     portal.AngleXZ = p.angle
     portal.DrawInfo.DrawNum = 160
     portal.DrawInfo.Flags = portal.DrawInfo.Flags | (1<<7)
     ensure_thing_on_ground(portal)
     centre_coord3d_on_block(portal.Pos.D3)
+  end
+  
+  function p:init_holo()
+    local holo_level = createThing(T_GENERAL,M_GENERAL_MAPWHO_THING,TRIBE_HOSTBOT,p.coord,false,false)
+    local holo_difficulty = createThing(T_GENERAL,M_GENERAL_MAPWHO_THING,TRIBE_HOSTBOT,p.coord,false,false)
+    centre_coord3d_on_block(holo_level.Pos.D3)
+    centre_coord3d_on_block(holo_difficulty.Pos.D3)
+    holo_level.Pos.D3.Ypos = holo_level.Pos.D3.Ypos + 512 + 128
+    holo_level.DrawInfo.DrawNum = p.holo_spr
+    holo_level.DrawInfo.Alpha = -16
+    holo_difficulty.Pos.D3.Ypos = holo_difficulty.Pos.D3.Ypos + 512 + 16
+    holo_difficulty.DrawInfo.DrawNum = p.holo_diff
+    holo_difficulty.DrawInfo.Alpha = -16
   end
   
   function p:process()
@@ -65,7 +80,8 @@ IPortal.reg  = function(_level, _angle, _x, _z)
     end
   end
   
-  p:initialize()
+  p:init_portal()
+  p:init_holo()
   
   return p
 end
