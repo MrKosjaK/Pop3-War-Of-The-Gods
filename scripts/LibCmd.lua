9037ff3864b0949d6805569a3942c67c8e56e0e2
MrKosjaK <misterkosiak@gmail.com>
2020-07-06 15:00:34 +0300

    LibCmd.lua updated

--- scripts/LibCmd.lua
+++ scripts/LibCmd.lua
@@ -4,6 +4,19 @@ import(Module_Defines)
 import(Module_Objects)
 import(Module_Map)
 
+function cmd_attack_thing(target)
+  local cmd = Commands.new()
+  if (target.Type == T_BUILDING) then
+    cmd.CommandType = CMD_ATTACK_AREA_2
+    cmd.u.TIdxSize.MapIdx = world_coord2d_to_map_idx(target.Pos.D2)
+  end
+  if (target.Type == T_PERSON) then
+    cmd.CommandType = CMD_ATTACK_TARGET
+    cmd.u.TargetIdx:set(target.ThingNum)
+  end
+  return cmd
+end
+
 function cmd_gather_wood(tree,flag)
   local cmd = Commands.new()
   cmd.Flags = cmd.Flags | CMD_FLAG_WOOD_TREE
